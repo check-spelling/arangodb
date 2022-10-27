2d899f12efd0fb485f0dcd3d122bff6209f961f6
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-27 06:40:14 -0400

    spelling: thread
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- client-tools/Import/AutoTuneThread.cpp
+++ client-tools/Import/AutoTuneThread.cpp
@@ -133,7 +133,7 @@ void AutoTuneThread::paceSends() {
 
   std::this_thread::sleep_until(_nextSend);
 
-  // if the previous send thread thread was found really quickly,
+  // if the previous send thread was found really quickly,
   //  assume arangodb is absorbing data faster than current rate.
   //  try doubling rate by halving pace time for subsequent send.
   if (!nextReset && _pace / 2 < _nextSend - now) {
