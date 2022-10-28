9bab10561c8326723d53a35cf4ef88aebb25dc99
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-28 05:10:13 -0400

    spelling: milliseconds
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- arangod/Agency/Node.cpp
+++ arangod/Agency/Node.cpp
@@ -472,7 +472,7 @@ ResultT<std::shared_ptr<Node>> Node::handle<SET>(VPackSlice const& slice) {
   VPackSlice ttl_v = slice.get("ttl");
   if (!ttl_v.isNone()) {
     if (ttl_v.isNumber()) {
-      // ttl in millisconds
+      // ttl in milliseconds
       long ttl = 1000l * ((ttl_v.isDouble())
                               ? static_cast<long>(ttl_v.getNumber<double>())
                               : static_cast<long>(ttl_v.getNumber<int>()));
