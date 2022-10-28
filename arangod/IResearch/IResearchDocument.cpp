9eef2787723dd053bea47a2de3f36111c3d9bd16
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-28 04:54:32 -0400

    spelling: initial
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- arangod/IResearch/IResearchDocument.cpp
+++ arangod/IResearch/IResearchDocument.cpp
@@ -97,7 +97,7 @@ std::initializer_list<irs::type_info::type_id> NumericStreamFeatures{
 
 // appends the specified 'value' to 'out'
 void append(std::string& out, size_t value) {
-  auto const size = out.size();  // intial size
+  auto const size = out.size();  // initial size
   out.resize(size + 21);         // enough to hold all numbers up to 64-bits
   auto const written = sprintf(&out[size], IR_SIZE_T_SPECIFIER, value);
   out.resize(size + written);
