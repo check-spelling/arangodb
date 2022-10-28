e6be3a569d03a52e1eec4791d2a30ed962d0b3de
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-28 05:11:34 -0400

    spelling: optimization
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- arangod/Replication/DatabaseInitialSyncer.cpp
+++ arangod/Replication/DatabaseInitialSyncer.cpp
@@ -2499,7 +2499,7 @@ arangodb::Result DatabaseInitialSyncer::fetchInventory(VPackBuilder& builder) {
     url += "&includeFoxxQueues=true";
   }
 
-  // use an optmization here for shard synchronization: only fetch the
+  // use an optimization here for shard synchronization: only fetch the
   // inventory including a single shard. this can greatly reduce the size of
   // the response.
   if (ServerState::instance()->isDBServer() && !_config.isChild() &&
